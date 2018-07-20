d367fe85a62aed35c66adf927ca3d11437f7a9af
FSxOmega <41387833+FSxOmega@users.noreply.github.com>
2018-07-20 13:35:57 +0200

    Update start.js

--- start.js
+++ start.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
+    message.reply('I got Updated!')
     console.log('Bot Launched!:D');
 });
 
