908b2d06c1a7add4ccd803f5893b067f82f827a3
FSxOmega <41387833+FSxOmega@users.noreply.github.com>
2018-07-20 20:32:36 +0200

    Update start.js

--- start.js
+++ start.js
@@ -17,7 +17,7 @@ client.on('message', message => {
 
 //Command +help
 client.on('message', message => {
-    if (message.content === prefix + 'help') {
+    if (message.content === '+help') {
 //         message.reply('Damn... here you got them - ' + prefix + 'info ' + prefix + 'yt ' + prefix + 'invite ' + prefix + 'donate ' + prefix + 'support // More Commands will come soon!');
       }  
 });
